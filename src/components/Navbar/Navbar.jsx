5dd3c0f7349df14ec387e081fd4a3b2f9e6a5247
sbtech24 <bajomosemilore@gmail.com>
2024-07-04 19:20:20 +0100

    responsive ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Link from 'next/link'
 import styles from "./navbar.module.css"
 
+import spotify from "/public/spotify.svg"
+import Image from 'next/image'
+
 const Navbar = () => {
   return (
     <div className={styles.container}>
         <nav className={styles.nav}>
            <Link href="/" className={styles.link}>Lorey</Link>
-           <Link href="spotify.com" className={styles.link} passHref={true}>Spotify</Link>       
+           <Link href="spotify.com" className={styles.link} passHref={true}>
+          <div className={styles.spotify}>
+          <Image src={spotify}/>
+          <p>spotify</p>
+          </div>
+           
+           </Link>       
         </nav>
     </div>
   )
